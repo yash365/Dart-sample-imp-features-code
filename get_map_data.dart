7694f7eda45fee46c4db65bfb68482a4df0c2bbf
Yash Jain <yash.jain6350@gmail.com>
2021-02-06 17:17:54 +0530

    getting the map data

---
+++ get_map_data.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  var _results = {
+  'Key_1' : 'Value_1',
+  'Key_2' : 'Value_2', 
+};
+  
+ var _list = _results.values.toList();
+ print(_list[1]);
+ print(_results["Key_2"]);
+
+ }
